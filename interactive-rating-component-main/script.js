3cf6faa27d3af6d87b4b82c98b68bdc4ff2e56df
Quelvinmp <quelvinmp@gmail.com>
2022-12-30 13:13:43 -0300

    implements click event listener for evaluator numbers to receive class "selected"

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -2,33 +2,49 @@ const rateContainer = document.getElementById('rate-container');
 const rateNumbers = document.getElementsByClassName('rate-number');
 
 const mouseoverItem = document.getElementsByClassName('mouseover');
+const selectedNumber = document.getElementsByClassName('selected');
+
+const submitButton = document.getElementById('submit-button');
 
 // /\ GLOBAL ELEMENTS /\ \\
-
-// const changeColorRatingNumbers = () => {
-//     for (let index = 0; index < rateNumbers.length; index += 1) {
-//         rateNumbers[index].addEventListener('mouseover', () => {
-//             if (mouseoverItem.length > 0) {
-//                 mouseoverItem[0].classList.remove('mouseover');
-//                 rateNumbers[index].classList.add('mouseover');
-//             } else {
-//                 rateNumbers[index].classList.add('mouseover');
-//             }
-//         })
-//     }
-// }
 
 const changeColorRatingNumbers = () => {
     for (let index = 0; index < rateNumbers.length; index += 1) {
         rateNumbers[index].addEventListener('mouseover', () => {
-            rateNumbers[index].classList.add('mouseover');
+            rateNumbers[index].classList.add('mouseoverNumbers');
             rateNumbers[index].addEventListener('mouseout', () => {
-                rateNumbers[index].classList.remove('mouseover')
+                rateNumbers[index].classList.remove('mouseoverNumbers')
             });
+        });
+    }
+};
+
+const changeColorSubmitButton = () => {
+    submitButton.addEventListener('mouseover', () => {
+        submitButton.classList.remove('mouseoutSubmit');
+        submitButton.classList.add('mouseoverSubmit');
+        submitButton.addEventListener('mouseout', () => {
+            submitButton.classList.remove('mouseoverSubmit');
+            submitButton.classList.add('mouseoutSubmit');
+        });
+    });
+};
+
+const changeColorSelectedRatingNumber = () => {
+    for (let index = 0; index < rateNumbers.length; index += 1) {
+        rateNumbers[index].addEventListener('click', () => {
+            if (selectedNumber.length > 0) {
+                selectedNumber[0].classList.remove('selected');
+                rateNumbers[index].classList.add('selected');
+            } else {
+                rateNumbers[index].classList.add('selected');
+            }
         });
     }
 };
 
 window.onload = () => {
     changeColorRatingNumbers();
+    changeColorSubmitButton();
+    changeColorSelectedRatingNumber();
 };
